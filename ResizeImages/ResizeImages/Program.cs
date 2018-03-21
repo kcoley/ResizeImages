5cce40146a05f6ee32430671606902c87c88757c
Kacey Coley <kcoley@users.noreply.github.com>
2018-03-20 18:22:57 -0700

    wip

--- ResizeImages/ResizeImages/Program.cs
+++ ResizeImages/ResizeImages/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
 using SixLabors.ImageSharp;
 using McMaster.Extensions.CommandLineUtils;
 
@@ -13,12 +16,34 @@ namespace ResizeImages
         /// <param name="width">The width to resize to.</param>
         /// <param name="height">The height to resize to.</param>
         /// <returns>The resized image.</returns>
-        public static void ResizeImages(string imageURL, int width, int height)
+        private static void ResizeImage(string imageURL, int width, int height)
         {
-            var resultImage = "output.png";
-            var image = Image.Load(imageURL);
-            image.Mutate(ctx => ctx.Resize(width, height));
-            image.Save(resultImage);
+            // var resultImage = "output.png";
+            // var image = Image.Load(imageURL);
+            // image.Mutate(ctx => ctx.Resize(width, height));
+            // image.Save(resultImage);
+        }
+
+        public static void ResizeImages(string manifestFile, int width, int height)
+        {
+            using (StreamReader stream = new StreamReader(manifestFile))
+            {
+                var jsonData = stream.ReadToEnd();
+                dynamic items = JsonConvert.DeserializeObject(jsonData);
+
+                foreach(var item in items)
+                {
+                    Console.WriteLine(item);
+                    Console.WriteLine("///");
+                }
+            }
+
+        }
+
+        private static int FailWithMessage(string message)
+        {
+            Console.WriteLine(message);
+            return -1;
         }
 
         static int Main(string[] args)
@@ -34,14 +59,57 @@ namespace ResizeImages
             {
                 if (manifestOption.HasValue())
                 {
-                    Console.WriteLine(manifestOption.Value());
+                    var manifestFile = manifestOption.Value();
+                    if (File.Exists(manifestFile))
+                    {
+                        if (widthOption.HasValue())
+                        {
+                            var width = 0;
+                            if (Int32.TryParse(widthOption.Value(), out width))
+                            {
+                                if (heightOption.HasValue())
+                                {
+                                    var height = 0;
+                                    if (Int32.TryParse(heightOption.Value(), out height))
+                                    {
+                                        Console.WriteLine(manifestOption.Value());
+                                        ResizeImages(manifestOption.Value(), Int32.Parse(widthOption.Value()), Int32.Parse(heightOption.Value()));
+                                    }
+                                    else
+                                    {
+                                        return FailWithMessage("Height value is not an integer!");
+                                    }
+                                }
+                                else
+                                {
+                                    return FailWithMessage("Height is missing!");
+                                }
+                            }
+                            else
+                            {
+                                return FailWithMessage("Width value is not an integer!");
+                            }
+                        }
+                        else
+                        {
+                            return FailWithMessage("Width is missing!");
+                        }
+                    }
+                    else
+                    {
+                        return FailWithMessage("Manifest file not found!");
+                    }
                 }
-                
+                else
+                {
+                    return FailWithMessage("Missing glTF manifest file!");
+                }
+
                 Console.WriteLine("Hello World");
                 return 0;
             });
 
-          
+
             return app.Execute(args);
         }
     }
